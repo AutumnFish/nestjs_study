87ea4b5e50e07f8a17200a4d2e5ac1050f2bba82
胡凌皓 <hulinghao@itcast.cn>
2019-12-28 18:01:08 +0800

    注册并使用中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { PostsModule } from './module/posts/posts.module';
+import { DemoMiddleware } from './core/middlewares/demo.middleware';
 
 @Module({
   imports: [PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+// 注册中间件
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(DemoMiddleware).forRoutes("posts")
+  }
+}
--- src/core/middlewares/demo.middleware.ts
+++ src/core/middlewares/demo.middleware.ts
@@ -2,6 +2,7 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 
 @Injectable()
 export class DemoMiddleware implements NestMiddleware {
+  // 在请求和响应之间增加执行的逻辑
   use(req: any, res: any, next: () => void) {
     // 打印内容
     console.log("hello ~~")
