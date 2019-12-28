0307c3442d9d4ba5c5db6ee8db858ef88d4e7e3e
胡凌皓 <hulinghao@itcast.cn>
2019-12-28 18:11:02 +0800

    完成过滤器内部逻辑

--- src/core/filters/demo.filter.ts
+++ src/core/filters/demo.filter.ts
@@ -1,6 +1,22 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 
-@Catch()
+@Catch(HttpException)
 export class DemoFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+  // 必须要实现catch方法
+  catch(exception: HttpException, host: ArgumentsHost) {
+    // 获取信息
+    const ctx = host.switchToHttp();
+    // 获取响应
+    const response =  ctx.getResponse();
+    // 获取请求
+    const request = ctx.getRequest();
+    // 获取状态
+    const status = exception.getStatus();
+
+    // 响应状态
+    response.status(status).json({
+      statusCode:status,
+      path:request.url
+    })
+  }
 }
