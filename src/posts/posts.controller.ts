7d8895d15eb423e8b340dd59ac74ef9edff6d777
胡凌皓 <hulinghao@itcast.cn>
2019-12-28 16:15:30 +0800

    获取查询字符串 以及获取 头部信息

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Query, Header, Headers } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
@@ -9,5 +9,34 @@ export class PostsController {
         // return "postController-index请求";
         // 如果是 对象或者数组 会被自动转换
         return {name:'jack',age:18}
+    }
+
+    // 获取请求的值
+    @Get('request')
+    // @Req 装饰器，获取 请求的所有参数
+    request(@Req() request){
+        // ip地址
+        console.log(request.ip)
+        // 查询字符串 
+        console.log(request.query)
+        // 请求的方法
+        console.log(request.method)
+        // 响应的内容
+        return 'query路由'
+    }
+
+    // 获取请求的查询字符串 key=value&key2=value2
+    @Get('query')
+    query(@Query() query){
+        
+        return '你发送过来的参数是:'+JSON.stringify(query)
+    }
+
+    // 获取提交的头部信息
+    @Get('headers')
+    headers(@Headers() headers){
+        console.log(headers)
+        return '获取头部信息'
+
     }
 }
