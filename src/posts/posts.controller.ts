ea2f03bd13eaa2913e0e83c675f720bd5f439b37
胡凌皓 <hulinghao@itcast.cn>
2019-12-28 15:39:06 +0800

    返回对象

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+    // get请求
+    @Get('index')
+    index(){
+        // 字符串直接返回
+        // return "postController-index请求";
+        // 如果是 对象或者数组 会被自动转换
+        return {name:'jack',age:18}
+    }
+}
