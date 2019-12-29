c009fe7e4a0f00b77163d8806d59126a4e887a37
胡凌皓 <hulinghao@itcast.cn>
2019-12-29 09:44:46 +0800

    在中间件中判断并添加属性

--- src/core/middlewares/demo.middleware.ts
+++ src/core/middlewares/demo.middleware.ts
@@ -6,6 +6,24 @@ export class DemoMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     // 打印内容
     console.log("hello ~~")
+
+
+    // 获取设置用户信息
+    req.user = {
+      roles:[
+        'guest'
+      ]
+    }
+
+    // 判断token
+    if(req.headers("token")==='secret'){
+      req.user={
+        roles:[
+          'member'
+        ]
+      }
+    }
+
     next();
   }
 }
