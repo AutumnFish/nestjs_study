7ab34ff997482e7ec47654551ce5aa7046ff3be8
胡凌皓 <hulinghao@itcast.cn>
2019-12-29 09:15:49 +0800

    注册全局守卫

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,24 @@ import { AppService } from './app.service';
 
 import { PostsModule } from './module/posts/posts.module';
 import { DemoMiddleware } from './core/middlewares/demo.middleware';
+import { APP_GUARD } from '@nestjs/core';
+import { DemoRolesGuard } from './core/guards/demo-roles.guard';
 
 @Module({
   imports: [PostsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      // 注册使用的守卫 为全局
+      useClass: DemoRolesGuard,
+    },
+  ],
 })
 // 注册中间件
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(DemoMiddleware).forRoutes("posts")
+    consumer.apply(DemoMiddleware).forRoutes('posts');
   }
 }
--- src/core/guards/demo-roles.guard.ts
+++ src/core/guards/demo-roles.guard.ts
@@ -1,11 +1,16 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { read } from 'fs';
 
 @Injectable()
 export class DemoRolesGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    // return false
+    // return true;
+    const req = context.switchToHttp().getRequest()
+
+    return req.headers['token']==='secret'
   }
 }
